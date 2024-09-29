a217fb8ebc11760c9be7f6299462166b4f21789c
Suddhu <suddhubharti@gmail.com>
2024-09-29 12:25:39 +0530

    add prisma to project

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -12,17 +12,23 @@ export default function DashboardLayout({
     <div className="h-screen flex">
       {/* LEFT */}
       <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] overflow-scroll">
-        <Link href="" className="flex items-center justify-center lg:justify-start gap-2 p-2">
-        <Image src="/logo.png" alt="logo" width={32} height={32}/>
-        <span className="hidden lg:block font-bold">SchoolExpert</span>
+        <Link
+          href=""
+          className="flex items-center justify-center lg:justify-start gap-2 p-2"
+        >
+          <Image src="/logo.png" alt="logo" width={32} height={32} />
+          <div className="hidden lg:flex flex-col">
+            <span className="font-bold text-sm font-sans">SPARRONIX</span>
+            <span className="relative left-7 text-xs">SchoolExpert</span>
+          </div>
         </Link>
-        <Menu/>
+        <Menu />
       </div>
       {/* RIGHT */}
       <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll flex flex-col">
-        <Navbar/>
+        <Navbar />
         {children}
       </div>
-      </div>
+    </div>
   );
 }
